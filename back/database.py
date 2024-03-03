9feabb4186ee410827c11de52fec7de52a0274c9
NogueraMateo <mateo.edu.co@gmail.com>
2024-03-03 00:06:23 -0500

    Puse una ruta relativa a la base de datos, antes habia una ruta absoluta lo que dificultaba ejecutar el proyecto en diferentes computadores

--- back/database.py
+++ back/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///C:/Users/Aura R/OneDrive - correounivalle.edu.co/Python/Gestor-Tareas/datos/usuarios-tareas.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///datos/usuarios-tareas.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread" : False}
